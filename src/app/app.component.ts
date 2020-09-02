def7c44d818e7964c84939ba089b13972be42247
German Alvaredo <geralvaredo@github.com>
2020-09-01 22:42:43 -0300

    Suma y promedio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab4-app01';
+  edad1: number;
+  edad2: number;
+  two = 2;
+
+  private obtenerEdades(): void {
+    this.edad1 = parseInt((document.getElementById('num1') as HTMLInputElement).value, 10);
+    this.edad2 = parseInt((document.getElementById('num2') as HTMLInputElement).value, 10);
+  }
+
+  btnCalculoSumaPromedio(): void {
+    this.obtenerEdades();
+    (document.getElementById('suma') as HTMLInputElement).value = (this.edad1 + this.edad2).toString();
+    (document.getElementById('promedio') as HTMLInputElement).value = ((this.edad1 + this.edad2) / this.two).toString();
+  }
+
+  btnLimpiar(): void {
+    (document.getElementById('num1') as HTMLInputElement).value = ' ';
+    (document.getElementById('num2') as HTMLInputElement).value = ' ';
+    (document.getElementById('suma') as HTMLInputElement).value = ' ';
+    (document.getElementById('promedio') as HTMLInputElement).value = ' ';
+  }
+
+
 }
